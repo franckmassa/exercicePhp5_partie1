2aec4fa3ab4e3cc759e75d6827f468251d9e5ff7
massa <massafranck@gmail.com>
2018-07-25 20:55:28 +0200

    initial

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$number = NULL;
+$number = 5;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice5</title>
+</head>
+<body>
+  <p>
+    <?php
+    echo $number;
+    ?>
+  </p>
+</body>
+</html>
